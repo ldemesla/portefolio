4bf7b97a54c198413439b33099ec719d0d13435f
ldemesla <ldemesla@student.42.fr>
2020-04-18 16:09:42 +0200

    fi navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,12 +11,16 @@ class NavBar extends React.Component {
             <div className="NavBar">
                 <h1> Louis Demeslay </h1>
                 <div className="Menu">
-                    <h1 onClick={()=>window.scrollTo(0, this.props.refs[1].current.offsetTop)}> Projects </h1>
-                    <h1 onClick={()=>window.scrollTo(0, this.props.refs[2].current.offsetTop)}> Skills </h1>
-                    <h1 onClick={()=>window.scrollTo(0, this.props.refs[0].current.offsetTop)}> About me </h1>  
+                    <h1 onClick={()=>this.scrollTo(this.props.refs[1])}> Projects </h1>
+                    <h1 onClick={()=>this.scrollTo(this.props.refs[2])}> Skills </h1>
+                    <h1 onClick={()=>this.scrollTo(this.props.refs[0])}> About me </h1>  
                 </div>
             </div>
         );
+    }
+    scrollTo(ref)
+    {
+        window.scrollTo(0, ref.current.offsetTop - (window.innerHeight * 0.1))
     }
 }
 
